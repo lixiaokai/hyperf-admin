5b8222348f92915c814f907df292142b4673e025
李小凯 <lixiaokai@live.com>
2023-01-03 00:05:19 +0800

    增加：service 抽象类

---
+++ core/Service/AbstractService.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core\Service;
+
+use Hyperf\Di\Annotation\Inject;
+use Hyperf\Logger\LoggerFactory;
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * 服务 - 抽象类.
+ */
+abstract class AbstractService
+{
+    protected LoggerInterface $logger;
+
+    /**
+     * @Inject
+     */
+    protected EventDispatcherInterface $eventDispatcher;
+
+    public function __construct(LoggerFactory $loggerFactory)
+    {
+        // 第 1 个参数对应日志的 name
+        // 第 2 个参数对应 config/autoload/logger.php 内的 key
+        $this->logger = $loggerFactory->get('Service');
+    }
+}
