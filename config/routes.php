10fd09e3c1cd6cdabd1774759e709c325058d5b6
李小凯 <lixiaokai@live.com>
2023-01-02 21:14:32 +0800

    增加：注释

--- config/routes.php
+++ config/routes.php
@@ -2,6 +2,13 @@
 
 declare(strict_types=1);
 
+/**
+ * 路由 - 配置文件.
+ *
+ * 说明：使用注解的方式注册路由，该配置忽略即可.
+ * @see https://hyperf.wiki/2.2/#/zh-cn/router
+ */
+
 use Hyperf\HttpServer\Router\Router;
 
 // 只保留网站 icon 图标的 get 请求
