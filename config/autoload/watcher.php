3f7a84559f8a34cc2ea856f70b6742b943d66280
李小凯 <lixiaokai@live.com>
2023-01-02 21:10:52 +0800

    增加：热更新、composer 命令

---
+++ config/autoload/watcher.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * 热更新 - 配置文件.
+ *
+ * 不足：
+ * 1. 删除文件和修改 .env 需要手动重启才能生效
+ * 2. vendor 中的文件需要使用 classmap 形式自动加载才能被扫描 ( 即执行 composer dump-autoload -o )
+ */
+
+use Hyperf\Watcher\Driver\ScanFileDriver;
+
+return [
+    'driver' => ScanFileDriver::class,
+    'bin' => 'php',
+    'watch' => [
+        'dir' => ['app', 'common', 'config', 'demo'],
+        'file' => ['.env'],
+        'scan_interval' => 2000,
+    ],
+];
