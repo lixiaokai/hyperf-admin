6b006ee62fc6731fa02326f02290cd55e15d36d7
李小凯 <lixiaokai@live.com>
2023-01-02 18:01:19 +0800

    自定义：PhpCsFixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,18 +1,26 @@
 <?php
 
-$header = <<<'EOF'
-This file is part of Hyperf.
+declare(strict_types=1);
 
-@link     https://www.hyperf.io
-@document https://hyperf.wiki
-@contact  group@hyperf.io
-@license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+/**
+ * PhpCsFixer.
+ *
+ * @see https://github.com/FriendsOfPHP/PHP-CS-Fixer/blob/master/doc/rules/index.rst 内置规则
+ */
+
+$header = <<<'EOF'
 EOF;
 
 return (new PhpCsFixer\Config())
-    ->setRiskyAllowed(true)
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->exclude('public')
+            ->exclude('runtime')
+            ->exclude('vendor')
+            ->in(__DIR__)
+    )
     ->setRules([
-        '@PSR2' => true,
+        '@PSR2' => true, // 遵循 PSR2
         '@Symfony' => true,
         '@DoctrineAnnotation' => true,
         '@PhpCsFixer' => true,
@@ -23,24 +31,26 @@ return (new PhpCsFixer\Config())
             'location' => 'after_declare_strict',
         ],
         'array_syntax' => [
-            'syntax' => 'short'
+            'syntax' => 'short',
         ],
         'list_syntax' => [
-            'syntax' => 'short'
+            'syntax' => 'short',
         ],
         'concat_space' => [
-            'spacing' => 'one'
+            'spacing' => 'one',
         ],
         'blank_line_before_statement' => [
             'statements' => [
                 'declare',
             ],
         ],
+        // phpDocs 中应该省略已经配置的注释
         'general_phpdoc_annotation_remove' => [
             'annotations' => [
-                'author'
+                'author',
             ],
         ],
+        // use 排序
         'ordered_imports' => [
             'imports_order' => [
                 'class', 'function', 'const',
@@ -57,7 +67,7 @@ return (new PhpCsFixer\Config())
             'identical' => false,
         ],
         'phpdoc_align' => [
-            'align' => 'left',
+            'align' => 'vertical', // 注释对齐方式
         ],
         'multiline_whitespace_before_semicolons' => [
             'strategy' => 'no_multi_line',
@@ -66,26 +76,20 @@ return (new PhpCsFixer\Config())
             'case' => 'lower',
         ],
         'class_attributes_separation' => true,
-        'combine_consecutive_unsets' => true,
+        'combine_consecutive_unsets' => true, // 多个 unset 合并成一个
         'declare_strict_types' => true,
         'linebreak_after_opening_tag' => true,
         'lowercase_static_reference' => true,
-        'no_useless_else' => true,
-        'no_unused_imports' => true,
+        'no_useless_else' => true, // 删除无用的 else
+        'no_unused_imports' => true, // 删除未使用的 use 语句
         'not_operator_with_successor_space' => true,
         'not_operator_with_space' => false,
-        'ordered_class_elements' => true,
+        'ordered_class_elements' => true, // class elements 排序
         'php_unit_strict' => false,
         'phpdoc_separation' => false,
-        'single_quote' => true,
+        'single_quote' => true,  // 简单字符串应该使用单引号代替双引号
         'standardize_not_equals' => true,
         'multiline_comment_opening_closing' => true,
     ])
-    ->setFinder(
-        PhpCsFixer\Finder::create()
-            ->exclude('public')
-            ->exclude('runtime')
-            ->exclude('vendor')
-            ->in(__DIR__)
-    )
+    ->setRiskyAllowed(true)
     ->setUsingCache(false);
