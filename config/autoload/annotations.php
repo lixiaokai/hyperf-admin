90b9039d38e35d0a7892e6b5ff707a40cd07308a
李小凯 <lixiaokai@live.com>
2023-01-02 21:20:18 +0800

    完善：注解配置

--- config/autoload/annotations.php
+++ config/autoload/annotations.php
@@ -1,19 +1,20 @@
 <?php
 
 declare(strict_types=1);
+
 /**
- * This file is part of Hyperf.
- *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ * 注解 - 配置文件.
  */
+
 return [
     'scan' => [
+        // 注解扫描的目录
         'paths' => [
             BASE_PATH . '/app',
+            BASE_PATH . '/common',
+            BASE_PATH . '/demo',
         ],
+        // 忽略的注解名
         'ignore_annotations' => [
             'mixin',
         ],
