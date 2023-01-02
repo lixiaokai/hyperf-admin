f07550d557cea64df06e8536580f3251abb7d322
李小凯 <lixiaokai@live.com>
2023-01-02 21:17:41 +0800

    增加：注释

--- config/autoload/async_queue.php
+++ config/autoload/async_queue.php
@@ -1,27 +1,27 @@
 <?php
 
 declare(strict_types=1);
+
 /**
- * This file is part of Hyperf.
+ * Redis 消息异步队列 - 配置文件.
  *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ * @see https://hyperf.wiki/2.2/#/zh-cn/async-queue
  */
+
 return [
     'default' => [
         'driver' => Hyperf\AsyncQueue\Driver\RedisDriver::class,
         'redis' => [
-            'pool' => 'default',
+            'pool' => 'default', // redis 连接池
         ],
-        'channel' => '{queue}',
-        'timeout' => 2,
-        'retry_seconds' => 5,
-        'handle_timeout' => 10,
-        'processes' => 1,
+        'channel' => '{queue}',  // 队列前缀
+        'timeout' => 2,          // pop 消息的超时时间
+        'retry_seconds' => 5,    // 失败后重新尝试间隔 ( 秒 ) ，也可传入数组，根据重试次数相应修改重试时间，例如 [1, 5, 10, 20]
+        'handle_timeout' => 10,  // 消息处理超时时间 ( 秒 )
+        'processes' => 1,        // 消费进程数
         'concurrent' => [
-            'limit' => 10,
+            'limit' => 10,       // 同时处理消息数
         ],
+        'max_messages' => 0,     // 进程重启所需最大处理的消息数 ( 默认 0-不重启 )
     ],
 ];
--- config/autoload/listeners.php
+++ config/autoload/listeners.php
@@ -1,13 +1,18 @@
 <?php
 
 declare(strict_types=1);
+
 /**
- * This file is part of Hyperf.
+ * 监听器 - 配置文件.
  *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ * 说明：
+ * 1. 自定义监听器注册通常使用注解的方式
+ * 2. 这里一般使用的是内置的监听器，避免新建一个文件去继承原来的监听器，并在新建的文件上使用注解
+ *
+ * 顺序
+ * 1. 这里配置的监听器触发顺序根据该配置文件的顺序
+ * 2. 注解注册监听器通过设置注解属性 priority 属性定义当前监听器的顺序，数字越大优先级越高
  */
+
 return [
 ];
