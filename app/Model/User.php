dccba4a612a9e30226f1f46d969390c1b4cae1b5
李小凯 <lixiaokai@live.com>
2023-01-04 00:49:11 +0800

    增加：用户模型、仓库

---
+++ app/Model/User.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model;
+
+use Core\Model\BaseModel;
+
+/**
+ * 用户信息 - 模型.
+ *
+ * @property int    $id     自增 ID
+ * @property string $name   名称
+ * @property string $phone  手机号
+ * @property string $status 状态 ( enable-启用 disable-禁用 )
+ */
+class User extends BaseModel
+{
+    public $timestamps = false;
+
+    protected $table = 'user';
+
+    protected $fillable = ['id', 'name', 'phone', 'status'];
+
+    protected $casts = ['id' => 'integer'];
+}
---
+++ app/Repository/UserRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Model\User;
+use Core\Repository\BaseRepository;
+use Hyperf\Database\Model\Collection;
+
+/**
+ * 用户信息 - 仓库类.
+ *
+ * @method User              getById(string $id)
+ * @method Collection|User[] getByIds(array $ids, array $columns = ['*'])
+ * @method User              create(array $data)
+ * @method User              update(User $model, array $data)
+ */
+class UserRepository extends BaseRepository
+{
+    protected string $modelClass = User::class;
+}
