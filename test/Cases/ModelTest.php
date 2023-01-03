450d0919ff21f78d1a25ec35cd5b69d590b86015
李小凯 <lixiaokai@live.com>
2023-01-03 11:20:29 +0800

    增加：模型单元测试

---
+++ test/Cases/ModelTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HyperfTest\Cases;
+
+use Core\Model\BaseModel;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * 模型 - 单元测试.
+ *
+ * @internal
+ * @coversNothing
+ */
+class ModelTest extends TestCase
+{
+    /**
+     * 测试 - 获取带限定表名的字段.
+     */
+    public function testQualifyColumn()
+    {
+        $model = new class() extends BaseModel {
+            protected $connection = 'default';
+            protected $table = 'user';
+        };
+
+        self::assertSame('user.id', $model->qualifyColumn('id'));
+        self::assertSame('user.id', $model::column('id'));
+        self::assertSame('base.user.id', $model::column('id', true));
+    }
+}
