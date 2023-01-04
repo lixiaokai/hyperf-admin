c767bf039bf3f1f97061d7a3f8574390991b88ad
李小凯 <lixiaokai@live.com>
2023-01-04 13:55:11 +0800

    去掉：公共异常记录 error

--- kernel/Exception/Handler/CommonExceptionHandler.php
+++ kernel/Exception/Handler/CommonExceptionHandler.php
@@ -6,36 +6,21 @@ namespace Kernel\Exception\Handler;
 
 use Hyperf\ExceptionHandler\ExceptionHandler;
 use Hyperf\HttpMessage\Stream\SwooleStream;
-use Hyperf\Logger\LoggerFactory;
 use Hyperf\Utils\Codec\Json;
 use Kernel\Exception\BaseException;
 use Psr\Http\Message\ResponseInterface;
-use Psr\Log\LoggerInterface;
 
 /**
  * 公共 - 异常处理器.
  */
 class CommonExceptionHandler extends ExceptionHandler
 {
-    protected LoggerInterface $logger;
-
-    public function __construct(LoggerFactory $loggerFactory)
-    {
-        $this->logger = $loggerFactory->get('Exception');
-    }
-
     /**
      * @param BaseException $throwable
      */
     public function handle(\Throwable $throwable, ResponseInterface $response): ResponseInterface
     {
         $this->stopPropagation();
-
-        $this->logger->error($throwable->getMessage(), [
-            'line' => $throwable->getLine(),
-            'file' => $throwable->getFile(),
-            'stack' => $throwable->getTrace(),
-        ]);
 
         return $response
             ->withHeader('Content-Type', 'application/json; charset=utf-8')
