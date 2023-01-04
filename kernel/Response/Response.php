9f45892584d7fc85641145347fef609f79a109ae
李小凯 <lixiaokai@live.com>
2023-01-04 13:55:39 +0800

    创建：响应类

---
+++ kernel/Response/Response.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kernel\Response;
+
+use Hyperf\HttpMessage\Stream\SwooleStream;
+use Hyperf\Utils\Codec\Json;
+use Hyperf\Context\Context;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * 响应类.
+ */
+class Response
+{
+    /**
+     * @var mixed
+     */
+    protected $data;
+
+    protected string $message = '';
+
+    /**
+     * @param mixed $data
+     */
+    public function __construct($data = [], string $message = '')
+    {
+        $this->data = $data;
+        $this->message = $message;
+    }
+
+    public static function withEmpty(): ResponseInterface
+    {
+        return (new self())->toJson();
+    }
+
+    /**
+     * @param mixed $data
+     */
+    public static function withData($data, string $message = ''): ResponseInterface
+    {
+        return (new self($data))->toJson();
+    }
+
+    /**
+     * @param mixed $data
+     */
+    public static function success($data): ResponseInterface
+    {
+        return (new self($data, '操作成功'))->toJson();
+    }
+
+    protected function toJson(): ResponseInterface
+    {
+        return $this->response()
+            ->withStatus(200)
+            ->withAddedHeader('content-type', 'application/json; charset=utf-8')
+            ->withBody(new SwooleStream(Json::encode([
+                'code' => 200,
+                'message' => $this->message,
+                'data' => $this->data,
+            ])));
+    }
+
+    protected function response(): ResponseInterface
+    {
+        return Context::get(ResponseInterface::class);
+    }
+}
