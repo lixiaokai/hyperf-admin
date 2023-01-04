a90ac91bbe9dd08640397afc27a78f1d1c4368d0
李小凯 <lixiaokai@live.com>
2023-01-04 20:35:14 +0800

    修改：异步消费进程实现的方式为配置方式

--- config/autoload/processes.php
+++ config/autoload/processes.php
@@ -1,13 +1,19 @@
 <?php
 
 declare(strict_types=1);
+
 /**
- * This file is part of Hyperf.
+ * 自定义进程 - 配置.
  *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ * 说明：配置方式和注解方式，二选一即可。
+ *
+ * @see https://hyperf.wiki/2.2/#/zh-cn/process 自定义进程
+ * @see https://hyperf.wiki/2.2/#/zh-cn/async-queue 异步队列
  */
+
 return [
+    // 任务调度器进程
+    // Hyperf\Crontab\Process\CrontabDispatcherProcess::class,
+    // 异步消费进程
+    Hyperf\AsyncQueue\Process\ConsumerProcess::class,
 ];
