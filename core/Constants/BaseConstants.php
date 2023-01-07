4c2a28256d9acfee62b0c21592d3c08328643721
李小凯 <lixiaokai@live.com>
2023-01-08 00:06:57 +0800

    增加：枚举常量

---
+++ core/Constants/BaseConstants.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core\Constants;
+
+/**
+ * 枚举常量 - 基类.
+ */
+class BaseConstants extends \Kernel\Constants\BaseConstants
+{
+}
---
+++ core/Constants/Status.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Core\Constants;
+
+use Hyperf\Constants\Annotation\Constants;
+
+/**
+ * 通用状态 - 常量.
+ *
+ * @Constants
+ *
+ * @method static getColor(string $code)
+ * @method static getText(string $code)
+ */
+class Status extends BaseConstants
+{
+    /**
+     * 通用状态 - 启用.
+     *
+     * @Color("green")
+     * @Text("启用")
+     */
+    public const ENABLE = 'enable';
+
+    /**
+     * 通用状态 - 禁用.
+     *
+     * @Color("red")
+     * @Text("禁用")
+     */
+    public const DISABLE = 'disable';
+}
