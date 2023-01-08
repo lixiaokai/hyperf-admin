02c173a7e9e9d48d0f83ab03094750c74003318b
李小凯 <lixiaokai@live.com>
2023-01-08 12:36:20 +0800

    完善：异常日志

--- kernel/Exception/Handler/CommonExceptionHandler.php
+++ kernel/Exception/Handler/CommonExceptionHandler.php
@@ -6,21 +6,33 @@ namespace Kernel\Exception\Handler;
 
 use Hyperf\ExceptionHandler\ExceptionHandler;
 use Hyperf\HttpMessage\Stream\SwooleStream;
+use Hyperf\Logger\LoggerFactory;
 use Hyperf\Utils\Codec\Json;
 use Kernel\Exception\BaseException;
+use Kernel\Exception\DataSaveException;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Log\LoggerInterface;
 
 /**
  * 公共 - 异常处理器.
  */
 class CommonExceptionHandler extends ExceptionHandler
 {
+    protected LoggerInterface $logger;
+
+    public function __construct(LoggerFactory $loggerFactory)
+    {
+        $this->logger = $loggerFactory->get('Exception');
+    }
+
     /**
      * @param BaseException $throwable
      */
     public function handle(\Throwable $throwable, ResponseInterface $response): ResponseInterface
     {
         $this->stopPropagation();
+
+        $this->log($throwable);
 
         return $response
             ->withHeader('Content-Type', 'application/json; charset=utf-8')
@@ -34,5 +46,22 @@ class CommonExceptionHandler extends ExceptionHandler
     public function isValid(\Throwable $throwable): bool
     {
         return $throwable instanceof BaseException;
+    }
+
+    /**
+     * 记录 - 日志.
+     */
+    protected function log(\Throwable $throwable): void
+    {
+        $message = sprintf('%s on line %s in %s', $throwable->getMessage(), $throwable->getLine(), $throwable->getFile());
+
+        switch (true) {
+            case $throwable instanceof DataSaveException:
+                $message .= PHP_EOL . $throwable->getTraceAsString();
+                $this->logger->error($message);
+                break;
+            default:
+                $this->logger->warning($message);
+        }
     }
 }
