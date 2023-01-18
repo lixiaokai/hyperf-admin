f5d9ea8e4209da8c44ac1fe004b7486d1ae30476
李小凯 <lixiaokai@live.com>
2023-01-18 18:53:48 +0800

    完善：菜单服务类

--- core/Service/Rbac/MenuService.php
+++ core/Service/Rbac/MenuService.php
@@ -6,13 +6,17 @@ namespace Core\Service\Rbac;
 
 use Core\Constants\ContextKey;
 use Core\Constants\Platform;
+use Core\Constants\Status;
 use Core\Model\Menu;
 use Core\Model\User;
 use Core\Repository\MenuRepository;
 use Core\Service\BaseService;
 use Hyperf\Context\Context;
+use Hyperf\Database\Model\Builder;
 use Hyperf\Database\Model\Collection;
+use Hyperf\Database\Model\Relations\BelongsToMany;
 use Hyperf\Di\Annotation\Inject;
+use Hyperf\Utils\Collection as UCollection;
 use Kernel\Exception\BusinessException;
 use Kernel\Helper\TreeHelper;
 
@@ -27,9 +31,11 @@ class MenuService extends BaseService
     /**
      * 获取 - 某终端平台权限菜单.
      *
+     * @param  string            $platform 终端平台
+     * @param  null|string       $status   菜单状态 ( 为 null 时获取所有状态 )
      * @return Collection|Menu[]
      */
-    public function list(string $platform = null, string $status = null): Collection
+    public function list(string $platform = Platform::ADMIN, string $status = null): Collection
     {
         return $this->repo->getList($platform, $status);
     }
@@ -38,8 +44,10 @@ class MenuService extends BaseService
      * 获取 - 某终端平台权限菜单树.
      *
      * 说明：默认获取总后台.
+     *
+     * @param null|string $platform 终端平台 ( 为 null 时表示总后台 )
      */
-    public function trees(string $platform = null): array
+    public function trees(string $platform = Platform::ADMIN): array
     {
         switch ($platform) {
             case Platform::TENANT: // 租户后台
@@ -55,23 +63,42 @@ class MenuService extends BaseService
     /**
      * 获取 - 某用户的权限菜单树.
      *
-     * @param null|User $user 用户模型 ( 为 null 时获取当前登录用户的 )
+     * @param string    $platform 终端平台
+     * @param null|User $user     用户模型 ( 为 null 时获取当前登录用户的 )
      * @see MenuServiceTest::testUserTrees()
      */
-    public function userTrees(User $user = null): array
+    public function userTrees(string $platform = Platform::ADMIN, User $user = null): array
     {
         // 1. 获取用户
-        if (is_null($user)) {
-            $user = Context::get(ContextKey::USER);
-        }
+        is_null($user) && $user = Context::get(ContextKey::USER);
 
-        // 2. 获取用户菜单
-        $menus = $user->isSuperAdmin() ? $this->list(Platform::ADMIN) : $user->menus();
-
-        // Todo: 用户获取菜单的方式不对，待修改成根据应用方式
-        // Todo: 过滤应用到期或关闭等菜单
+        // 2. 获取用户某终端平台菜单
+        $menus = $user->isSuperAdmin() ? $this->list() : $this->userMenus($platform, $user);
 
         return TreeHelper::toTrees($menus->toArray());
+    }
+
+    public function userMenus(string $platform = Platform::ADMIN, User $user = null): UCollection
+    {
+        is_null($user) && $user = Context::get(ContextKey::USER);
+
+        return $user->roles
+            ->load(['menus' => function (BelongsToMany $query) use ($platform) {
+                // Todo: 也可以考虑把这里的过滤条件放到获取之后再过滤
+                // 某平台
+                $query->when(Platform::has($platform), fn (Builder $query) => $query->where(Menu::column('platform'), $platform));
+                // 某状态
+                $query->where(Menu::column('status'), Status::ENABLE);
+                // 租户终端平台时检查 appId
+                $query->when($platform === Platform::TENANT, function (Builder $query) {
+                    $tenant = Context::get(ContextKey::TENANT);
+                    $tenantIds = $tenant->apps->where('status', Status::ENABLE)->keyBy('id'); // Todo: 进一步进行过滤，比如是否过期等
+                    $query->whereIn(Menu::column('app_id'), $tenantIds);
+                });
+            }])
+            ->pluck('menus')
+            ->flatten()
+            ->unique('id');
     }
 
     /**
--- test/Service/Rbac/MenuServiceTest.php
+++ test/Service/Rbac/MenuServiceTest.php
@@ -2,9 +2,12 @@
 
 namespace HyperfTest\Service\Rbac;
 
+use Core\Constants\ContextKey;
 use Core\Constants\Platform;
+use Core\Model\Tenant;
 use Core\Model\User;
 use Core\Service\Rbac\MenuService;
+use Hyperf\Context\Context;
 use PHPUnit\Framework\TestCase;
 
 class MenuServiceTest extends TestCase
@@ -21,14 +24,28 @@ class MenuServiceTest extends TestCase
      */
     public function testUserTrees()
     {
+        self::assertTrue(true);
+    }
+
+    /**
+     * @see MenuService::userTrees()
+     */
+    public function testAdminTrees()
+    {
         $user = User::find(1);
-        $trees = $this->menuService->userTrees($user);
+        $trees = $this->menuService->userTrees(Platform::ADMIN, $user);
         self::assertSame(Platform::ADMIN, $trees[0]['platform']);
+    }
+
+    /**
+     * @see MenuService::userTrees()
+     */
+    public function testTenantTrees()
+    {
+        Context::set(ContextKey::TENANT, Tenant::find(1));
 
         $user = User::find(2);
-        $trees = $this->menuService->userTrees($user);
+        $trees = $this->menuService->userTrees(Platform::TENANT, $user);
         self::assertSame(Platform::TENANT, $trees[0]['platform']);
-
-        self::assertTrue(true);
     }
 }
