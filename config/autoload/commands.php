8ee535505a0ec0a159683365855fe208157fb3b4
李小凯 <lixiaokai@live.com>
2023-02-24 16:42:41 +0800

    完善命令行配置中文注释

--- config/autoload/commands.php
+++ config/autoload/commands.php
@@ -1,13 +1,10 @@
 <?php
 
 declare(strict_types=1);
+
 /**
- * This file is part of Hyperf.
- *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ * 命令行 - 配置文件.
  */
+
 return [
 ];
